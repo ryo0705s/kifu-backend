3f40f8f8077fc8503df2a38085f235c2de004180
ryo0705s <inspire.sasaki15@gmail.com>
2023-08-28 01:41:23 +0900

    django-filterを追加し、フロントからのクエリをフィルターしてレスポンスを返せるように変更

--- kifu/kifu_app/apis.py
+++ kifu/kifu_app/apis.py
@@ -4,6 +4,8 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.generics import ListAPIView
 # from rest_framework.generics import ListCreateAPIView
 from .serializers import UserSerializer,DonationSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
   
 class createDonation(CreateAPIView):
   queryset = donation.objects.all()
@@ -27,3 +29,15 @@ class getUser(RetrieveAPIView):
 class getUserList(ListAPIView):
   queryset = user.objects.all()
   serializer_class = UserSerializer
+  filter_backends = [DjangoFilterBackend,SearchFilter]
+  filterset_fields = ['mail','password']
+  search_fields = ['mail','password']
+  # def get_queryset(self):
+  #   queryset = user.objects.all()
+  #   usermail = self.request.query_params.get('mail', None)
+  #   userpassword = self.request.query_params.get('password', None)
+  #   if usermail is not None:
+  #     queryset = queryset.filter(mail=usermail)
+  #   if userpassword is not None:
+  #     queryset = queryset.filter(password=userpassword)
+  #   return queryset
--- kifu/kifu_app/serializers.py
+++ kifu/kifu_app/serializers.py
@@ -5,7 +5,9 @@ class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = user
     fields = ['id', 'mail', 'password', 'total_donation_amounts']
+    read_only_fields = ['id']
 class DonationSerializer(serializers.ModelSerializer):
   class Meta:
     model = donation
     fields = ['id', 'user','donation_amount']
+    read_only_fields = ['id']
