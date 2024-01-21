efb7d1c6d94410d511bbdf0b98c3fa86f89e91e8
ryo0705s <inspire.sasaki15@gmail.com>
2024-01-21 18:06:31 +0900

    feat: サイレントリフレッシュ用のエンドポイントを二つ作成

--- kifu/kifu_app/urls.py
+++ kifu/kifu_app/urls.py
@@ -4,7 +4,9 @@ from django.conf.urls import include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView,
 )
+from dj_rest_auth.jwt_auth import get_refresh_view
 
 urlpatterns = [
   path('api/createDonation', apis.createDonation.as_view(), name='createDonation'),
@@ -18,6 +20,7 @@ urlpatterns = [
   path('rest-auth/registration/', include('rest_auth.registration.urls')),
   path('rest-auth/google/', apis.GoogleLogin.as_view(), name='google_login'),
   path('api/login/', apis.CustomLoginView.as_view(), name='custom_login'),
-  path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-  path("api/auth/", include("dj_rest_auth.urls"))
+  path("api/auth/", include("dj_rest_auth.urls")),
+  path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+  path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
