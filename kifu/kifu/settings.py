8ef424afa5dea1eda05ac5c4513c4ce9c0ac8b47
ryo0705s <inspire.sasaki15@gmail.com>
2024-04-14 23:28:31 +0900

    refactor: 起動不具合などのためバージョン変更等実施

--- kifu/kifu/settings.py
+++ kifu/kifu/settings.py
@@ -155,7 +155,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
-        # 'rest_framework_simplejwt.authentication.JWTAuthentication', 
+        'rest_framework_simplejwt.authentication.JWTAuthentication', 
         
     )
 }   
